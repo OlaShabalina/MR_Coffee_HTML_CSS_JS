1352cb2304c123d84ad9b688b503c53d9f965e2b
OlaShabalina <o.shabalina@outlook.com>
2021-08-16 12:23:20 +1000

    Completed form validation

--- form.js
+++ form.js
@@ -15,12 +15,9 @@ const message = document.querySelector('#message');
 
 // here are events to submit form and open a pop-up
 
-
 contactForm.addEventListener('submit', (e) => {
     e.preventDefault();
-    // checkInputs();
-    popup.classList.add('active');
-    overlay.classList.add('active');
+    checkInputs();
 })
 
 
@@ -34,12 +31,17 @@ function checkInputs() {
     const messageValue = message.value.trim();
 // used .trim() to remove white spaces
 
+    let count = 0; // count represents points of validation
+    let dataStorage = {};
+
 // Validating name first
     if (nameValue === '') {
         // show error by adding error class or success with success class, I'll create separate functions for it below
         setErrorFor(username, "Le nom d’utilisateur ne peut pas être vide") // translation: Username cannot be blank
     } else {
         setSuccessFor(username);
+        count++
+        dataStorage.username = username;
     }
 
 // Validating surname the same way as name
@@ -48,6 +50,8 @@ function checkInputs() {
         setErrorFor(surname, "Le nom de famille ne peut pas être vide") // translation: Surname cannot be blank
     } else {
         setSuccessFor(surname);
+        count++
+        dataStorage.surname = surname;
     }
 
 //Validating phone numer 
@@ -55,6 +59,7 @@ function checkInputs() {
         setErrorFor(phone, "Veuillez saisir un numéro de téléphone valide"); // translation: Please enter a valid phone number
     } else {
         setSuccessFor(phone);
+        dataStorage.phone = phone;
     }
 
 // Validating email 
@@ -64,6 +69,8 @@ function checkInputs() {
         setErrorFor(email, "Veuillez saisir une adresse e-mail valide"); // translation: Please enter a valid email address
     } else {
         setSuccessFor(email);
+        count++
+        dataStorage.email = email;
     }
 
 // Validating message
@@ -72,12 +79,24 @@ function checkInputs() {
         setErrorFor(message, "Veuillez ajouter un message"); // translation: Please add a message
     } else {
         setSuccessFor(message)
+        count++
+        dataStorage.message = message;
     }
 
-// show the pop-up if everything is successful
+// show the pop-up if everything is successful, I'll use count for that
 
-    
-
+    if (count === 4) {
+        console.log(dataStorage);
+            // reset colors of input once the form is submitted
+        resetSuccessError(username);
+        resetSuccessError(surname);
+        resetSuccessError(phone);
+        resetSuccessError(email);
+        resetSuccessError(message);
+        // create a pop-up
+        popup.classList.add('active');
+        overlay.classList.add('active');
+    }
 }
 
 
@@ -95,6 +114,12 @@ function setErrorFor (input, message) {
 function setSuccessFor (input) {
     const formElement = input.parentElement; 
     formElement.classList.add('success');
+}
+
+function resetSuccessError (input) {
+    const formElement = input.parentElement; 
+    formElement.classList.remove('success');
+    formElement.classList.remove('error');
 }
 
 //Function to validate phone will be here
